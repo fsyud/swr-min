3668f38482ec872b2d708e9b87f8dd317aea9e28
董宇(技术中心/基金研发部) <yu.dong1@yintech.cn>
2023-05-05 11:24:07 +0800

    feat: 打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,35 @@
-import typescript from "rollup-plugin-typescript2";
-import pkg from "./package.json";
+import rollupTypescript from "@rollup/plugin-typescript"; //
+import resolve from "@rollup/plugin-node-resolve"; // 告诉 Rollup 如何处理在代码中使用的导入语句（例如 import 和 require）
+import commonjs from "@rollup/plugin-commonjs"; // commonjs 模块转成es6 模块
+import babel from "@rollup/plugin-babel"; // es6 -> es5
 
 // 一段自定义的内容，以下内容会添加到打包结果中
 const footer = `
 if(typeof window !== 'undefined') {
-  window._Ho_HOOKS_VERSION_ = '${pkg.version}'
+  window._Ho_HOOKS_VERSION_ = 1.0.0
 }`;
 
 export default {
-  input: "src/main.ts",
+  input: "src/index.ts",
   output: [
     {
+      file: "dist/index.esm.js",
+      format: "esm",
+      footer,
+    },
+    {
       name: "use_swr",
-      file: pkg.main,
+      file: "dist/index.browser.js",
       format: "umd",
-      sourcemap: true,
       footer,
     },
   ],
-  plugins: [typescript(), clear({ targets: ["dist"] })],
+  plugins: [
+    rollupTypescript(),
+    commonjs(),
+    resolve(),
+    babel({
+      exclude: "node_modules/**",
+    }),
+  ],
 };
