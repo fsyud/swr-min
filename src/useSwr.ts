525efcae4404f66bd530f5ebe74619b6a5792243
董宇(技术中心/基金研发部) <yu.dong1@yintech.cn>
2023-05-08 16:32:51 +0800

    feat: add _internal

--- src/useSwr.ts
+++ src/useSwr.ts
@@ -1,14 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import useFetchConfigContext from "swr-min/_internal";
 
 interface useSwrProps {
   url: string;
   fetcher: any;
+  options?: {
+    fetcher?: any;
+  };
 }
 
-function useSwr({ url, fetcher }: useSwrProps) {
+function useSwr({ url, fetcher, options = {} }: useSwrProps) {
   const [data, setData] = useState<object | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
+
+  // 通过 useContext 获取 useFetchConfigContext 的全局配置
+  const config = Object.assign({}, useContext(useFetchConfigContext), options);
+
+  let fn = fetcher;
+
+  if (typeof fn === "undefined") {
+    // 使用全局配置的 fetcher
+    fn = config.fetcher;
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,7 +31,7 @@ function useSwr({ url, fetcher }: useSwrProps) {
 
       try {
         // 这里直接调用外部传进来的 fetcher，并使用 url 作为参数
-        const newData = await fetcher(url);
+        const newData = await fn(url);
         setData(newData);
       } catch (error) {
         setIsError(false);
